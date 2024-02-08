572e735380356fd4fb38c7b15a5577ee3a164810
Rohith <rohithpeddireddy24@gmail.com>
2024-02-08 23:29:35 +0530

    first commit

--- src/components/appointmentform/AppointmentForm.js
+++ src/components/appointmentform/AppointmentForm.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './appstyle.css'
 import { useForm } from 'react-hook-form'
-import { Label } from '@mui/icons-material'
 
 
 const AppointmentForm = () => {
@@ -15,17 +14,17 @@ const AppointmentForm = () => {
       </div>
         <form onSubmit={handleSubmit(onSubmit)} className='formdata'>
             <h2>Patient Appointment Form</h2>
-            <Label htmlFor='firstname'>Firstname:</Label>
+            <label for='firstname'>Firstname:</label>
             <input {...register('Firstname',{required:true,maxLength:14})} name='firstname'/><br/><br/>
-            <Label htmlFor='lastname'>Last name:</Label>
+            <label for='lastname'>Last name:</label>
             <input {...register('Lastname',{pattern:/^[A-Za-z]+$/i})} name='lastname'/><br/><br/>
-            <Label htmlFor='problem'>Problem:</Label>
+            <label for='problem'>Problem:</label>
             <input {...register('Problem',{pattern:/^[A-Za-z]+$/i})} name='problem'/><br/><br/>
-            <Label htmlFor='mobilenumber'>Mobile Number:</Label>
+            <label for='mobilenumber'>Mobile Number:</label>
             <input {...register('mobile number',{required:true,maxLength:10})} name='mobilenumber'/><br/><br/>
-            <Label htmlFor='age'>Age:</Label>
+            <label for='age'>Age:</label>
             <input type='number' {...register('Age',{max:99,min:21})} name='age'/><br/><br/>
-            <Label htmlFor='gender'>Gender:</Label>
+            <label for='gender'>Gender:</label>
             <input type='text' {...register('gender',{maxLength:6,required:true})} name='gender'/><br/><br/>
             <button className='formbtn'>Submit</button>
         </form>
